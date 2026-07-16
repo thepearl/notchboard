6eacc5a86353e40c66ff5d71ac3f03bb3f5eaec1
Ghazi Tozri <ghazi.tozri98@gmail.com>
2026-07-16 20:22:57 +0200

    Initial Commit

---
+++ notchboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notchboard
+//
+//  Created by Ghazi on 7/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notchboard/notchboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchboardApp.swift
+//  notchboard
+//
+//  Created by Ghazi on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct notchboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
